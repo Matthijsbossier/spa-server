5b9cd8fb206e3ca777d6ef0be30096df07e8d77c
Matthijs Bossier <mebossie@student.avans.nl>
2017-12-17 16:06:41 +0100

    aanpassingen

--- model/artist.model.js
+++ model/artist.model.js
@@ -13,22 +13,22 @@ const ArtistSchema = new Schema({
 
 const Artist = mongoose.model('artist', ArtistSchema);
 
-// Test: add an detailed artist every time the server starts.
-// const artist = new Artist({
-//     name: 'Bon Iver',
-//     description: '',
-//     imagePath: 'https://i.scdn.co/image/e759e8e517c8936035d6fb8a7535286937cf4e23',
-//     albums: [
-//         { name: 'For Emma, Forever Ago',
-//          description: 'First album',
-//          imagePath: 'https://img.discogs.com/IC3kOdrS1w-zSkOJTxWJghz_AGs=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1357686-1259230699.jpeg.jpg',
-//          year: 2009,
-//          songs: [ {
-//              name: 'Skinny Love',
-//              number: 3,
-//              length: '3:30'
-//          }] 
-//         }] 
-// }).save();
+//Test: add an detailed artist every time the server starts.
+const artist = new Artist({
+    name: 'Bon Iver',
+    description: '',
+    imagePath: 'https://i.scdn.co/image/e759e8e517c8936035d6fb8a7535286937cf4e23',
+    albums: [
+        { name: 'For Emma, Forever Ago',
+         description: 'First album',
+         imagePath: 'https://img.discogs.com/IC3kOdrS1w-zSkOJTxWJghz_AGs=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1357686-1259230699.jpeg.jpg',
+         year: 2009,
+         songs: [ {
+             name: 'Skinny Love',
+             number: 3,
+             length: '3:30'
+         }] 
+        }] 
+}).save();
 
 module.exports = Artist;
